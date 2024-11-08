e3622dd06fbdd5f2b1323277822f5eecddc60cd9
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2024-11-08 16:45:28 -0300

    FEAT: Creating respectives form request

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return True;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'emailInput'    => 'required|email',
+            'passwordInput' => 'required',
+        ];
+    }
+
+    public function messages():array
+    {
+        return [
+            'emailInput.required'    => 'Insira seu Email para realizar login',
+            'emailInput.email'       => 'O Email inserido deve ser válido',
+            'passwordInput.required' => 'Insira sua senha para realizar login',
+        ];
+    }
+}

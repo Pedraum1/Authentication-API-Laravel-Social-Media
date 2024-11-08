740afe4fbf996d0ad5ca76a72189a49b833d8f9c
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2024-11-08 16:47:26 -0300

    FEAT: Creating routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Classes\ApiClass;
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/status', function(){
-  return 'status ok';
+  return ApiClass::success('API is running');
 });
+
+Route::post('/login',[AuthenticationController::class,'login']);
+Route::post('/register',[AuthenticationController::class,'register']);
+Route::get('/validation/{token}',[AuthenticationController::class,'validatingEmail'])->name('validation');
